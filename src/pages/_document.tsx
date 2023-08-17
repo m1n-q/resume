2b4b59f5610a3881acfb7eb8c18a030f32045871
m1n-q <m1nq.dev@gmail.com>
2023-08-17 17:24:54 +0900

    apply missing fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,16 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://webfontworld.github.io/gmarket/GmarketSans.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
