9aa676c86a79de026ff1dcdba96dcb593b5b323d
m1n-q <m1nq.dev@gmail.com>
2023-11-13 00:05:10 +0900

    Revert "remove notice temporarily"
    This reverts commit 36734347250ecf384b79aa85bd1d5b064f53ab7e.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Projects from '../components/Projects';
 import OpenSource from '../components/Opensources';
 import AboutMe from '@/components/AboutMe';
@@ -9,19 +9,21 @@ import where2go from '@/lib/contents/where2go';
 import Line from '@/components/Line';
 import Educations from '@/components/Educations';
 import transcendence from '@/lib/contents/transcendence';
+import Notice from '@/components/Notice';
 
 export default function Home() {
-  // const [showNotice, setShowNotice] = useState(true);
-  //
-  // const handleCloseNotice = () => {
-  //   setShowNotice(false);
-  // };
+  const [showNotice, setShowNotice] = useState(true);
+
+  const handleCloseNotice = () => {
+    setShowNotice(false);
+  };
+
 
   return (
     <>
-      {/*{showNotice &&*/}
-      {/* <Notice text="배포 상태에 따라 일부 링크가 동작하지 않을 수 있습니다." onClose={handleCloseNotice} />*/}
-      {/*  }*/}
+    {showNotice &&
+     <Notice text="배포 상태에 따라 일부 링크가 동작하지 않을 수 있습니다." onClose={handleCloseNotice} />
+      }
       <AboutMe introduces={aboutMe.introduces} links={aboutMe.links} />
       <Line />
       <Projects infos={[where2go, transcendence]} />
