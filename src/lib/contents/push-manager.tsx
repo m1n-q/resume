3689ec4bb7d96f694aa574017f912c548b7aaa50
m1n-q <m1nq.dev@gmail.com>
2024-08-21 19:19:12 +0900

    프로젝트 정보 수정 - 링크 제거 - 설명에 개행 추가
    TODO: 레포지토리 및 페이지 링크 추가하기

--- src/lib/contents/push-manager.tsx
+++ src/lib/contents/push-manager.tsx
@@ -1,3 +1,4 @@
+import ExternalLinkIcon from '@/components/Icon/ExternalLinkIcon';
 import GithubIcon from '@/components/Icon/GithubIcon';
 import LinkWithIcon from '@/components/LinkWithIcon';
 import pushManagerTasks from '@/lib/contents/push-manager-tasks';
@@ -8,18 +9,15 @@ const stacks = {
 };
 
 const pushManager: ProjectInfo = {
-  name: 'push-manager',
+  name: 'push manager',
   period: '2024.08 -',
   team: '1인 개발',
   links: [
-    <LinkWithIcon
-      Icon={GithubIcon}
-      text={'Source'}
-      href={'https://github.com/over-the-inner-circle/transcendence'}
-    />,
+    <LinkWithIcon Icon={ExternalLinkIcon} text={'Demo'} href={'TODO'} />,
+    <LinkWithIcon Icon={GithubIcon} text={'Source'} href={'TODO'} />,
   ],
   role: ['AWS 서버리스 인프라 구축', 'Terraform 코드 작성'],
-  description: '설정한 루틴에 맞추어 OO 메신저에 메시지를 보내는 서버리스 앱',
+  description: '설정한 루틴에 맞추어,\nOO 메신저에 메시지를 보내는 서버리스 앱',
   techStack: stacks,
   tasks: pushManagerTasks,
 };
