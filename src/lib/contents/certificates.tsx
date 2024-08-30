9d3f3a34743bba9ed159ec88a276487524ef307d
m1n-q <m1nq.dev@gmail.com>
2024-08-30 21:29:21 +0900

    apply credly links

--- src/lib/contents/certificates.tsx
+++ src/lib/contents/certificates.tsx
@@ -4,14 +4,17 @@ const certificates: CertificateInfo[] = [
   {
     title: 'AWS SAA: Certified Solutions Architect Associate',
     badgeAsset: '/assets/aws-saa.png',
+    link: 'https://www.credly.com/badges/c6d46424-91f4-4fc4-916e-e1a89fe025ce/public_url',
   },
   {
     title: 'HashiCorp Certified Terraform Associate',
     badgeAsset: '/assets/terraform.png',
+    link: 'https://www.credly.com/badges/7494dc24-1546-40fb-87c2-24ac1f6b2151/public_url',
   },
   {
     title: 'CKA: Certified Kubernetes Administrator',
     badgeAsset: '/assets/cka.png',
+    link: 'https://www.credly.com/badges/325a8714-ba0d-4f45-90b7-c39f685ffde9/public_url',
   },
 ];
 
