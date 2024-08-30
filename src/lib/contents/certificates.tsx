54f4fc4e1f1dee2584bbdcf966971f894f833b14
m1n-q <m1nq.dev@gmail.com>
2024-08-30 20:34:49 +0900

    add CKA

--- src/lib/contents/certificates.tsx
+++ src/lib/contents/certificates.tsx
@@ -2,11 +2,14 @@ import CertificateInfo from '@/types/CertificateInfo';
 
 const certificates: CertificateInfo[] = [
   {
-    title: 'AWS Certified Solutions Architect - Associate',
+    title: 'AWS Certified Solutions Architect - Associate (SAA)',
     badgeAsset: '/assets/aws-saa-badge.png',
   },
   {
     title: 'HashiCorp Certified Terraform Associate',
+  },
+  {
+    title: 'Certified Kubernetes Administrator (CKA)',
   },
 ];
 
